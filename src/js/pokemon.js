b9f659c6af0e86927b96946fb6623217efc54c56
davidglnd <david.galindo.dgp@gmal.com>
2025-03-23 18:39:43 +0100

    feat: :sparkles: Añadida imagen FAV en js

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -3,28 +3,60 @@ import pokedex from '../pokedex/pokedex.json' with { type: "json" }
 
 window.addEventListener("DOMContentLoaded", onDOMContentLoaded)
 
+
 /**
  * Evento que se lanza cuando el contenido de la página ha sido cargado en memoria
  * y se puede acceder a él.
+ * 
  * @listens DOMContentLoaded
+ * @description
+ *  Asigna los eventos que se observan a partir de que cargue la página:
+ *  - Al formulario, se le asigna el evento "submit" que llama a la función
+ *    encontrarPokemon.
+ *  - Al botón de buscar, se le asigna el evento "click" que imprime un mensaje en
+ *    consola.
+ *  Luego, llama a la función leerListaPokemons con el parámetro 12 para pintar
+ *  los 12 primeros pokemons en la lista.
+ * 
+ * @returns {void}
  */
 function onDOMContentLoaded() {
     let formulario = document.getElementById('formulario')
-    let botonBuscar = document.getElementById('botonbusqueda')
+    //let botonBuscar = document.getElementById('botonbusqueda') //el boton es submit asi que no lo neceisto
+    let campoTexto = document.getElementById('input-buscador')
      // Asigno los eventos que se observan a partir de que cargue la página
     formulario.addEventListener('submit', encontrarPokemon)
-    botonBuscar.addEventListener('click', () => console.log('Boton buscar'))
-
+    //botonBuscar.addEventListener('click', () => console.log('Boton buscar')) //el boton es submit asi que no lo neceisto
+    campoTexto.addEventListener('keyup' , onInputKeyUp)
     // Leo la lista de pokemons y pinto el HTML
     leerListaPokemons(12)
+    console.log('Se ha cargado la pagina')
 }
 
 
+/**
+ * Lee la lista de pokemons y pinta el HTML
+ * 
+ * @description
+ *  Lee la lista de pokemons y pinta el HTML, el cual se mostrará en el contenedor
+ *  con el id "lista-pokemons".
+ *  Crea un nuevo "li" por cada pokemon y a su vez dentro de cada "li" crea un "figure" y
+ *  dentro de este crea una "img" y un "h5" que contienen la imagen y el nombre del pokemon
+ *  respectivamente.
+ *  Crea un "p" que contiene el número de pokedex del pokemon.
+ *  Crea un "div" que contiene los tipos del pokemon y cada tipo se muestra en un "span"
+ *  que se le da un estilo en base al tipo.
+ *  El parámetro "numeroPokemonLeidos" indica el número de pokemons que se van a leer.
+ *  Si no se indica el parámetro, por defecto se leen 10 pokemons.
+ * 
+ * @param {number} numeroPokemonLeidos - Número de pokemons a leer y pintar en el HTML.
+ * @returns {void}
+ */
 function leerListaPokemons(numeroPokemonLeidos = 10) {
     let listaPokemons = document.getElementsByClassName('lista-pokemons')[0]
     // Vacío la tabla antes de rellenar con los nuevos pokemons
     while (listaPokemons.firstChild) {
-        console.log('borrando')
+       // console.log('borrando')
         listaPokemons.removeChild(listaPokemons.firstChild) 
     }
     
@@ -35,8 +67,20 @@ function leerListaPokemons(numeroPokemonLeidos = 10) {
     
 }
 
-
-
+/**
+ * Busca un pokemon en la lista de pokemons y lo pinta en la tabla de pokemons.
+ * 
+ * @description
+ *  Se lanza cuando se envía el formulario de búsqueda.
+ *  Busca el pokemon en la lista de pokemons según:
+ *    - Si el valor del input es un número, busca por id.
+ *    - Si el valor del input es un texto, busca por nombre.
+ *  Si encuentra pokemons, los pinta en la tabla de pokemons.
+ *  Si no encuentra ninguno, avisa al usuario y sale.
+ * 
+ * @param {Event} event - Evento submit del formulario.
+ * @returns {void}
+ */
 function encontrarPokemon(event) {/*Le pasamos el evento submit que lo declaramos en cuanto carga la pagina*/
     //paramos evento submit
     event.preventDefault()
@@ -75,13 +119,24 @@ function encontrarPokemon(event) {/*Le pasamos el evento submit que lo declaramo
     }
 }
 
+/**
+ * Añade un pokemon a la lista de pokemons.
+ * 
+ * @description
+ *  Toma un pokemon como parámetro y lo pinta en la tabla de pokemons.
+ *  El pokemon se pinta con su nombre, su número de pokedex y sus tipos.
+ *  Si el pokemon tiene 2 tipos, se pintan los dos.
+ *  Si no tiene 2 tipos, no se pinta el segundo.
+ * @param {Object} numeroIndicePokemon - El pokemon a pintar.
+ * @returns {void}
+ */
 function addPokemonToList(numeroIndicePokemon){ /*el valor que recibimos aqui es pokedex[i] del array  por vuelta del for de leeListaPokemon*/
 
     let listaPokemons = document.getElementsByClassName('lista-pokemons')[0]
     let nuevoPokemon = document.createElement('li')
 
     nuevoPokemon.classList = 'caja-pokemon'	
-    console.log('Añadiendo pokemon :', numeroIndicePokemon.id + ' con nombre ' + numeroIndicePokemon.name.english)
+    //console.log('Añadiendo pokemon :', numeroIndicePokemon.id + ' con nombre ' + numeroIndicePokemon.name.english)
     listaPokemons.appendChild(nuevoPokemon)
 
     let pokemon = document.createElement('figure')
@@ -95,6 +150,7 @@ function addPokemonToList(numeroIndicePokemon){ /*el valor que recibimos aqui es
     imagenPokemon.setAttribute('alt',  numeroIndicePokemon.name.english)
     imagenPokemon.setAttribute('title', numeroIndicePokemon.name.english)
     imagenPokemon.setAttribute('loading', 'lazy')
+    imagenPokemon.setAttribute('class','imgPokemon')
     pokemon.appendChild(imagenPokemon)
 
     let nombrePokemon = document.createElement('h5')
@@ -125,9 +181,29 @@ function addPokemonToList(numeroIndicePokemon){ /*el valor que recibimos aqui es
         tipo2.classList ='tag ' + numeroIndicePokemon.type[1].toLowerCase()
         tipos.appendChild(tipo2)
     }
+//Aqui creamos una imagen que vamos a usar de boton para agregar a favoritos, quizas tengamos que añadir 
+// un form para enviar el input oculto con el id del pokemon que estamos añadiendo
+    let favorito = document.createElement('img')
+    favorito.id = numeroIndicePokemon.id
+    favorito.classList = 'enviar-favoritos'
+    favorito.src = '/img/favoritosinmarcar.png'
+    pokemon.appendChild(favorito)
+
+    let favoritoOculto = document.createElement('input')
+    favoritoOculto.id = 'oculto'
+    favoritoOculto.setAttribute('type' , 'hidden')
+    favoritoOculto.setAttribute('value' , numeroIndicePokemon.id)
+    pokemon.appendChild(favoritoOculto)
+    
+
+
 }
-
-
+function onInputKeyUp(event){
+    let buscador = document.getElementById('input-buscador')
+    if(buscador.value === ''){
+        leerListaPokemons(12)
+    }
+}
 
 
 
