d74d039deca7e18d74fccc014cb6ff5352cfeee2
davidglnd <david.galindo.dgp@gmal.com>
2025-03-25 18:04:46 +0100

    feat: :sparkles: Uso del fetch para un buscador formulariohtml)

---
+++ src/js/formulario.js
@@ -0,0 +1,97 @@
+import pokedex from '../pokedex/pokedex.json' with { type: "json" }
+
+window.addEventListener("DOMContentLoaded", onDOMContentLoaded)
+
+function onDOMContentLoaded() {
+    let formulario = document.getElementById('formulario')
+    formulario.addEventListener('submit', encontrarPokemon)
+}
+function encontrarPokemon(event){
+    let formulario = document.getElementById('input-buscador')
+    let listaInformacion = document.getElementsByClassName('lista-informacion')[0]
+
+    event.preventDefault()
+
+
+    if(formulario === ''){
+        let errorBusqueda = document.createElement('p')
+        errorBusqueda.innerText = 'Introduce un pokemon por id o nombre'
+        listaInformacion.appendChild(errorBusqueda)
+        return 
+    }
+    
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${formulario.value}`)
+        .then((respuesta) => { //respuesta es la informacion que nos devuelve la API
+        // Vemos si la respuesta es correcta
+        if (!respuesta.ok) {//Comprobamos si la respuesta es diferente de ok por lo que nos daria error
+            throw new Error(`HTTP error! Status: ${respuesta.status}`);
+        }
+        // Devolvemos la información en forma de JSON
+        return respuesta.json();
+        })
+        .then((informacionPokemonApi) => {
+
+            //console.log(formulario.value)
+            //console.log(informacionPokemonApi)
+            while (listaInformacion.firstChild) {
+                // console.log('borrando')
+                listaInformacion.removeChild(listaInformacion.firstChild) 
+             }
+            pintarPokemon(informacionPokemonApi)
+        })
+        // .catch((mensajeError) => {
+        //     console.log('sasd')
+        //     let error = document.createElement('p')
+        //     error.innerText = 'UPS... Algo ha salido mal, intentalo de nuevo'
+        //     console.log(error)
+        // })
+}
+
+function pintarPokemon(pokemon){
+    console.log(pokemon)
+    let listaInformacion = document.getElementsByClassName('lista-informacion')[0]
+
+    let informacionPokemon = document.createElement('li')
+    informacionPokemon.setAttribute('class', 'caja-pokemon-informacion')
+    
+    let figureInformacion = document.createElement('figure')
+    figureInformacion.setAttribute('class','figure-informacion')
+
+    let nombrePokemon = document.createElement('p')
+    nombrePokemon.setAttribute('class', 'nombre-pokemon')
+    nombrePokemon.innerText = pokemon.name
+
+    let generacionPokemon = document.createElement('p')
+    generacionPokemon.innerText = 'Generacion : ' + pokemon.generation.name
+
+    let legendary = document.createElement('p')
+    if(pokemon.is_legendary === false){
+        legendary.innerText = 'No es legendario'
+    } else {
+        legendary.innerText = 'Es legendario'
+    }
+
+    let grupoHuevos = document.createElement('p')
+    grupoHuevos.innerText = 'Su famila de huevos es: ' + pokemon.egg_groups.map((eggGroup) => eggGroup.name).join(', ')
+    
+    let tasaCrecimiento = document.createElement('p')
+    tasaCrecimiento.innerText = 'Tasa de crecimiento: ' + pokemon.growth_rate.name
+
+    let imagenPokemon = document.createElement('img')
+    imagenPokemon.setAttribute('src', '../pokedex/images/' + String(pokemon.id).padStart(3, '0') + '.png')
+    imagenPokemon.setAttribute('class','imgPokemon')
+
+    listaInformacion.appendChild(informacionPokemon)
+    informacionPokemon.appendChild(figureInformacion)
+    figureInformacion.appendChild(imagenPokemon)
+    figureInformacion.appendChild(nombrePokemon)
+    figureInformacion.appendChild(generacionPokemon)
+    figureInformacion.appendChild(legendary)
+    figureInformacion.appendChild(grupoHuevos)
+    figureInformacion.appendChild(tasaCrecimiento)
+
+
+
+
+
+}
